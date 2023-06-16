8c10e034d9feef9905e5db0e2e6effd61462d30e
taype-pruebas <136181227+taype-pruebas@users.noreply.github.com>
2023-06-16 12:17:34 -0500

    Archivos base

---
+++ src/main/java/certus/edu/escuelita/EscuelitaApplication.java
@@ -0,0 +1,13 @@
+package certus.edu.escuelita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscuelitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscuelitaApplication.class, args);
+	}
+
+}
