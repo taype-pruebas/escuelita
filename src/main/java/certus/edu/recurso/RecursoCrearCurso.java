f01c97fbad81195e58c11a3aed113b0c01bcc675
taype dev <jetaypedev@gmail.com>
2023-06-17 07:53:52 -0500

    merge create main and rama-diego

--- src/main/java/certus/edu/recurso/RecursoCrearCurso.java
+++ src/main/java/certus/edu/recurso/RecursoCrearCurso.java
@@ -3,14 +3,17 @@ package certus.edu.recurso;
 import certus.edu.escuelita.CrearCursoApplication;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Path("/RecursoCrearCursos")
+@Path("/recursocurso")
 public class RecursoCrearCurso {
     List<CrearCursoApplication> CrearCursoApplication=new ArrayList<CrearCursoApplication>();
     @GET
+    @Produces({MediaType.APPLICATION_JSON})
     public List<CrearCursoApplication> getCrearCursoApplication(){
         CrearCursoApplication .add(new CrearCursoApplication("Python", "Introduccion", 6) );
         CrearCursoApplication .add(new CrearCursoApplication("JavaScript", "Avanzado", 3) );
